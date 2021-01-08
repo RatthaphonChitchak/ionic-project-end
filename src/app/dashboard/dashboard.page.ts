405faf8297776c644dee60860694366fad75d478
Ratthaphon <toy_2537@msn.com>
2021-01-08 12:57:02 +0700

    loading

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,3 +1,4 @@
+import { LoadingService } from './../loading.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,15 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardPage implements OnInit {
   newsOnline: any;
   img: any;
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    public loading: LoadingService
+    ) { }
 
   ngOnInit() {
     this.getnews();
   }
-  getnews() {
-   this.http.get<any>('http://localhost:3010/api/newss').subscribe(res => {
+  async getnews() {
+    await this.loading.presentLoadingWithOptions();
+   this.http.get<any>('https://covid19news-00.herokuapp.com/api/newss').subscribe(res => {
       this.newsOnline = res.data;
       this.img = res.data.urlimg;
+      this.loading.dismissOnPageChange();
     });
   }
 
